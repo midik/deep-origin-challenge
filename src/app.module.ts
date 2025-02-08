f53c29ab22aeab68574d3b1c0491a62ca8ca73cb
mid <midiman@ya.ru>
2025-02-08 15:36:01 +0100

    added postgres and prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UrlShortenerModule } from './url-shortener/url-shortener.module';
-import { UrlShortenerService } from './url-shortener/url-shortener.service';
 import { UrlModule } from './url/url.module';
 import { UserModule } from './user/user.module';
 import { UrlTrackModule } from './url-track/url-track.module';
+import { DbModule } from './prisma-service/db.module';
 
 @Module({
-  imports: [UrlShortenerModule, UrlModule, UserModule, UrlTrackModule],
+  imports: [
+    UrlShortenerModule,
+    UrlModule,
+    UserModule,
+    UrlTrackModule,
+    DbModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, UrlShortenerService],
+  providers: [AppService],
 })
 export class AppModule {}
