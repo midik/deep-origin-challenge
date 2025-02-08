b0091941ce95dc57e2bc1715696fc5dbd7e7e112
mid <midiman@ya.ru>
2025-02-08 15:37:27 +0100

    resolve deps

--- src/url/url.module.ts
+++ src/url/url.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { UrlService } from './url.service';
 import { UrlController } from './url.controller';
+import { UrlShortenerService } from '../url-shortener/url-shortener.service';
+import { DbModule } from '../prisma-service/db.module';
 
 @Module({
   controllers: [UrlController],
-  providers: [UrlService],
+  providers: [UrlService, UrlShortenerService],
+  imports: [DbModule],
 })
 export class UrlModule {}
